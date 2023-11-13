d51213d6fc65673a09e5185d6eb3fdb0f1cfb08b
Kai O'Reilly <koreilly5297@gmail.com>
2023-11-12 22:00:17 -0800

    wrap the tracing functions so that people don't have to import gorqlite

---
+++ trace.go
@@ -0,0 +1,26 @@
+package rqlite
+
+import (
+	"io"
+
+	"github.com/rqlite/gorqlite"
+)
+
+// We wrap the tracing functions so that people don't have to import gorqlite.
+
+// TraceOn turns on tracing output to the io.Writer of your choice.
+//
+// Trace output is very detailed and verbose, as you might expect.
+//
+// Normally, you should run with tracing off, as it makes absolutely
+// no concession to performance and is intended for debugging/dev use.
+func TraceOn(w io.Writer) {
+	gorqlite.TraceOn(w)
+}
+
+// TraceOff turns off tracing output. Once you call TraceOff(), no further
+// info is sent to the io.Writer, unless it is TraceOn'd again.
+func TraceOff() {
+	gorqlite.TraceOff()
+
+}
